1d8c2866a8d08599e13109f97bf6fe8443cbffdd
oThinas <thiagomartinsprado@gmail.com>
2025-05-09 15:57:24 -0300

    chore: logging port

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from './app';
 import { env } from './env';
 
 app.listen({ port: env.PORT }).then(() => {
-  console.log('🚀 Server is running on http://localhost:3333');
+  console.log('🚀 Server is running on port:', env.PORT);
 });
