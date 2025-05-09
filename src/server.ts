f5c02abfc860b007d1f0bc029f5e58a968959ae8
oThinas <thiagomartinsprado@gmail.com>
2025-05-09 16:02:49 -0300

    chore: update host

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('🚀 Server is running on port:', env.PORT);
 });
