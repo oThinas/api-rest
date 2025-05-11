27b564a7abcb2a44ed0a2753de625dbaa29cb2eb
oThinas <thiagomartinsprado@gmail.com>
2025-05-11 20:15:33 -0300

    chore: update hostname to localhost

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app';
 import { env } from './env';
 
-app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
-  console.log('🚀 Server is running on port:', env.PORT);
-});
+app
+  .listen({
+    port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+  })
+  .then(() => {
+    console.log('🚀 Server is running on port:', env.PORT);
+  });
